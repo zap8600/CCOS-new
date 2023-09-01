6ed31f83975294fe820ead7edb4bc9d277717625
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-01 14:32:07 +0000

    change to ccos

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/ccos.kernel isodir/boot/ccos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "ccos" {
+	multiboot /boot/ccos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o ccos.iso isodir

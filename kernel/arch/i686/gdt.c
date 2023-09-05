458d71d7860a63e7bf892d373c65b25a8cf4d6d3
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-05 17:55:16 +0000

    remove gdt bc i dont know anymore

--- kernel/arch/i686/gdt.c
+++ kernel/arch/i686/gdt.c
@@ -2,45 +2,12 @@
 
 #include "gdt.h"
 #include <kernel/string.h>
-
-uint64_t gdt_entries[3];
-
-struct GDTR{
-	uint16_t limit;
-    uint64_t address;
-} __attribute__((packed));
-
-void
-create_descriptor(uint32_t base, uint32_t limit, uint16_t flag, int entry)
-{
-    uint64_t descriptor;
- 
-    // Create the high 32 bit segment
-    descriptor  =  limit       & 0x000F0000;         // set limit bits 19:16
-    descriptor |= (flag <<  8) & 0x00F0FF00;         // set type, p, dpl, s, g, d/b, l and avl fields
-    descriptor |= (base >> 16) & 0x000000FF;         // set base bits 23:16
-    descriptor |=  base        & 0xFF000000;         // set base bits 31:24
- 
-    // Shift by 32 to allow for low part of segment
-    descriptor <<= 32;
- 
-    // Create the low 32 bit segment
-    descriptor |= base  << 16;                       // set base bits 15:0
-    descriptor |= limit  & 0x0000FFFF;               // set limit bits 15:0
-
-	gdt_entries[entry] = descriptor << 32;
-}
+#include <kernel/stdio.h>
 
 void init_gdt() {
-	gdt_entries[0] = 0;
-	create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL0), 1);
-    create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL0), 2);
+	printf("No GDT set, kept for basic structure.\n");
 
-	struct GDTR gdtr = {
-		.limit = 3 * sizeof(uint64_t) - 1,
-		.address = (uintptr_t)&gdt_entries,
-	};
-
+	/*
     asm volatile (
 		"mov %0, %%edi\n"
 		"lgdt (%%edi)\n"
@@ -52,4 +19,5 @@ void init_gdt() {
 		"ltr %%ax\n"
 		: : "r"(&gdtr)
 	);
+	*/
 }

1ff44c7f12967be1b562f1f7b983062f4a185ad4
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-10-11 18:23:43 +0000

    wip i686 mem

--- kernel/arch/i686/mem.c
+++ kernel/arch/i686/mem.c
@@ -2,9 +2,24 @@
 #include <kernel/mem.h>
 
 extern uint32_t kernel_end;
+uint32_t placement_addr = (uint32_t)&kernel_end;
 
-uint32_t int_kmalloc(uint32_t size) {
-    
+// internal watermark allocator
+uint32_t wm_alloc_int(uint32_t size, int align, uint32_t *phys) {
+    if(align == 1 && (placement_addr & 0xFFFFF000)) {
+        placement_addr &= 0xFFFFF000;
+        placement_addr += 0x1000;
+    }
+    if (phys) {
+        *phys = placement_addr;
+    }
+    uint32_t tmp = placement_addr;
+    placement_addr += size;
+    return tmp;
+}
+
+uint32_t wm_alloc(uint32_t size) {
+    return wm_alloc_int(size, 0, 0);
 }
 
 void init_mem(void) {

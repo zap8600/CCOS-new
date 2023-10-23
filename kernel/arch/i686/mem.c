3783319903ce3cd5a7617453a938f49c5a327a3b
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-10-23 16:28:24 +0000

    i figured out what im doin

--- kernel/arch/i686/mem.c
+++ kernel/arch/i686/mem.c
@@ -1,26 +1,13 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <kernel/mem.h>
+#include "heap.h"
 
-extern uint32_t kernel_end;
-uint32_t placement_addr = (uint32_t)&kernel_end;
+static volatile uint32_t *frames;
+static size_t nframes;
 
-// internal watermark allocator
-uint32_t wm_alloc_int(uint32_t size, int align, uint32_t *phys) {
-    if(align == 1 && (placement_addr & 0xFFFFF000)) {
-        placement_addr &= 0xFFFFF000;
-        placement_addr += 0x1000;
-    }
-    if (phys) {
-        *phys = placement_addr;
-    }
-    uint32_t tmp = placement_addr;
-    placement_addr += size;
-    return tmp;
-}
-
-uint32_t wm_alloc(uint32_t size) {
-    return wm_alloc_int(size, 0, 0);
-}
+#define INDEX_FROM_BIT(b)  ((b) >> 5)
+#define OFFSET_FROM_BIT(b) ((b) & 0x1F)
 
 void init_mem(void) {
 }
